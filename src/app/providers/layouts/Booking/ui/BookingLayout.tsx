c759569f1d208b6f91ce454c34ecf9784517ba0f
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-07-31 19:17:56 +0300

    UPDATE: Updated PageSteps styles in BookingLayout

--- src/app/providers/layouts/Booking/ui/BookingLayout.tsx
+++ src/app/providers/layouts/Booking/ui/BookingLayout.tsx
@@ -1,11 +1,13 @@
 import { IChildren } from '@/shared/types/shared'
 import { PageSteps } from '@/shared/ui/PageSteps/PageSteps'
 import { FC } from 'react'
+import cls from './BookingLayout.module.scss'
 
 const BookingLayout: FC<IChildren> = ({ children }) => {
 	return (
 		<>
 			<PageSteps
+				className={cls.pageSteps}
 				steps={[
 					{ label: 'Номер и Цена', activePath: '/rooms' },
 					{ label: 'Бронь и Оплата', activePath: '/booking' },
