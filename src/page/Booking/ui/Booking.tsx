7ff09072dc91341c9bae0af8ac49d50c90d571f4
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-08-02 15:01:57 +0300

    UPDATE: Updated Booking page content

--- src/page/Booking/ui/Booking.tsx
+++ src/page/Booking/ui/Booking.tsx
@@ -1,11 +1,26 @@
 import { cn } from '@/shared/lib'
 import { IClassName } from '@/shared/types/shared'
+import { Typography } from '@/shared/ui/Typography/Typography'
+import { BookingLogoTitle } from '@/widgets/BookingLogoTitle'
 import { FC } from 'react'
 import cls from './Booking.module.scss'
 
 interface Props extends IClassName {}
 const Booking: FC<Props> = ({ className }) => {
-	return <div className={cn(cls.booking, [className])}>Booking</div>
+	const TITLE = '2-х местный'
+	const NIGHT_PRICE = 2996
+
+	return (
+		<div className={cn(cls.booking, [className])}>
+			<BookingLogoTitle />
+			<Typography weight='SB' className={cn(cls.title)}>
+				{TITLE}
+				<br />
+				<span>{NIGHT_PRICE},00 руб</span>
+			</Typography>
+			Booking
+		</div>
+	)
 }
 
 export default Booking
