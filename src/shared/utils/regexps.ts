488a6100f86ca08c60018d322b3f30fe3d78f4e9
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-08-04 11:44:23 +0300

    UPDATE: Updated name regex in utils

--- src/shared/utils/regexps.ts
+++ src/shared/utils/regexps.ts
@@ -1,4 +1,4 @@
-export const name: RegExp = /^[а-яА-Я]{2,20}$/
+export const name: RegExp = /^[а-яА-Яa-zA-Z]{2,20}$/
 export const mail: RegExp =
 	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 export const date: RegExp = /[\d-]{10,10}/
