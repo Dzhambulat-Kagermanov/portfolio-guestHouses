270459290e5eaad3e881c865898c5de333baf96d
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-08-07 11:58:45 +0300

    ADD: Added RoomsCard on HomePage

--- src/widgets/HomeRooms/ui/HomeRooms.tsx
+++ src/widgets/HomeRooms/ui/HomeRooms.tsx
@@ -1,15 +1,16 @@
+import { getCards } from '@/shared/api'
 import { cn } from '@/shared/lib'
 import { IClassName } from '@/shared/types/shared'
-import { Button } from '@/shared/ui/Button/Button'
 import { Container } from '@/shared/ui/Container/Container'
-import { InfoGroup } from '@/shared/ui/InfoGroup/InfoGroup'
 import { Slider } from '@/shared/ui/Slider/Slider'
 import { Typography } from '@/shared/ui/Typography/Typography'
+import { RoomsCard } from '@/widgets/RoomsCard'
 import { FC } from 'react'
 import cls from './HomeRooms.module.scss'
 
 interface Props extends IClassName {}
-const HomeRooms: FC<Props> = ({ className }) => {
+const HomeRooms: FC<Props> = async ({ className }) => {
+	const roomsData = await getCards()
 	return (
 		<Container
 			containerClass={cn(cls.container)}
@@ -23,7 +24,32 @@ const HomeRooms: FC<Props> = ({ className }) => {
 				slidesPerView={'auto'}
 				loop
 				spaceBetween={20}
-				items={[1, 2, 3, 4, 5, 6, 7]}
+				items={roomsData.data.map(
+					(
+						{
+							availableRooms,
+							maxGuests,
+							previewDescription,
+							previewImg,
+							services,
+							slug,
+							title,
+						},
+						index
+					) => (
+						<RoomsCard
+							key={index}
+							availableRooms={availableRooms}
+							maxGuests={maxGuests}
+							previewDescription={previewDescription}
+							previewImg={previewImg}
+							services={services}
+							slug={slug}
+							title={title}
+							className={cn(cls.roomCard)}
+						/>
+					)
+				)}
 				wrapperClass={cn(cls.sliderWrapper)}
 				className={cn(cls.slider)}
 				slideClass={cn(cls.slide)}
