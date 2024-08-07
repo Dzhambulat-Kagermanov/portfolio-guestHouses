ccf3e53eafa0e8d52e56af3a3181b163b4459933
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-08-07 15:52:49 +0300

    UPDATEL Updated useBookingFormData. Added persist middleware

--- src/shared/hooks/zustand/useBookingFormData.ts
+++ src/shared/hooks/zustand/useBookingFormData.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { devtools, persist } from 'zustand/middleware'
 
 export type TBookingForm = {
 	dateIn?: string
@@ -11,18 +12,46 @@ export type TBookingForm = {
 	guests?: number
 	isPayLater?: boolean
 	services?: string
+	aboutMeInfo?: string
 }
 interface IUseBookingFormData {
 	formContext: any
 	setValue: (change: keyof TBookingForm | 'formContext', value: any) => void
+	resetValues: () => void
 }
 
-export const useBookingFormData = create<IUseBookingFormData & TBookingForm>(
-	(set, get) => ({
-		formContext: undefined,
-		setValue: (change, value) =>
-			set(state => ({
-				[change]: value,
-			})),
-	})
+export const useBookingFormData = create<IUseBookingFormData & TBookingForm>()(
+	persist(
+		(set, get) => ({
+			formContext: undefined,
+			setValue: (change, value) =>
+				set(state => ({
+					[change]: value,
+				})),
+			resetValues: () =>
+				set(state => ({
+					dateIn: undefined,
+					dateOut: undefined,
+					email: undefined,
+					firstName: undefined,
+					formContext: undefined,
+					guests: undefined,
+					isPayLater: undefined,
+					patronymic: undefined,
+					phone: undefined,
+					secondName: undefined,
+					services: undefined,
+					aboutMeInfo: undefined,
+				})),
+		}),
+		{
+			name: 'bookingFormData',
+			partialize: state =>
+				Object.fromEntries(
+					Object.entries(state).filter(
+						([key]) => !['formContext'].includes(key)
+					)
+				),
+		}
+	)
 )
