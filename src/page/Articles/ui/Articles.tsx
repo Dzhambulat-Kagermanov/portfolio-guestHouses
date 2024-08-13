80643026a5e6cf0f670903bec7e1cb1e67f88790
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-08-13 22:25:38 +0300

    REFACTOR: Code in ArticlesPage was moved in ArticlesList

--- src/page/Articles/ui/Articles.tsx
+++ src/page/Articles/ui/Articles.tsx
@@ -1,10 +1,9 @@
-'use client'
 import { getArticles } from '@/shared/api'
 import { cn } from '@/shared/lib'
 import { arrayChunks } from '@/shared/lib/arrayChunks'
 import { IArticlesCardData } from '@/shared/types'
 import { IClassName } from '@/shared/types/shared'
-import { ArticleCard } from '@/widgets/ArticleCard'
+import { ArticlesList } from '@/widgets/ArticlesList'
 import { FC } from 'react'
 import cls from './Articles.module.scss'
 
@@ -13,23 +12,8 @@ const Articles: FC<Props> = async ({ className }) => {
 	const data = arrayChunks<IArticlesCardData>((await getArticles()).data, 2)
 
 	return (
-		<div className={cn(cls.articles, [className])}>
-			{data.map(array => {
-				return (
-					<div className={cn(cls.group)}>
-						{array.map(({ description, id, title }) => {
-							return (
-								<ArticleCard
-									key={id}
-									description={description}
-									title={title}
-									className={cn(cls.item)}
-								/>
-							)
-						})}
-					</div>
-				)
-			})}
+		<div className={cn(cls.articlesPage, [className])}>
+			<ArticlesList data={data} className={cn(cls.articles)} />
 		</div>
 	)
 }
