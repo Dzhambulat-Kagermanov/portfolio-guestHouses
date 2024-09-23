158f9d7f78451098545593e16d864d4ca420c8a8
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-09-23 10:07:43 +0300

    FIX: Fixed type error in arrayChunks

--- src/shared/lib/arrayChunks/index.ts
+++ src/shared/lib/arrayChunks/index.ts
@@ -1,5 +1,5 @@
 export const arrayChunks = <Array>(array: Array[], chunkSize: number) => {
-	const subarray: unknown[] = []
+	const subarray: Array[][] = []
 	for (let i = 0; i < Math.ceil(array.length / chunkSize); i++) {
 		subarray[i] = array.slice(i * chunkSize, i * chunkSize + chunkSize)
 	}
